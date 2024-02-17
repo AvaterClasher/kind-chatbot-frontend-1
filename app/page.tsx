61d0620d7aaa260f500c075df87b88b2b24b3580
AvaterClasher <avater.clasher@gmail.com>
2024-02-17 16:46:29 +0530

    feat: :sparkles: Added TextToSpeech component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+/** @format */
+
+import { ChatBotCanvas } from "@/components/ChatBotCanvas";
+import { IsPlayingProvider } from "./context/isPlayingContext";
+import { TextToSpeech } from "@/components/TextToSpeech";
+
 export default function Home() {
-  return (
-    <main className="h-screen">
-      
-    </main>
-  )
+	return (
+		<main className="h-screen">
+			<IsPlayingProvider>
+				<TextToSpeech />
+				<ChatBotCanvas />
+			</IsPlayingProvider>
+		</main>
+	);
 }
